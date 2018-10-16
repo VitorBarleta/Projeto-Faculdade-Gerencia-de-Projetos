9776050f5b1d8424a9d794e6683791cdde1095c5
VitorBarleta <vitorbarleta99@gmail.com>
2018-10-16 16:52:39 -0300

    Inicio do desenvolvimento da tela de notas

--- project/src/app/apps/fast-notes/fast-notes.component.ts
+++ project/src/app/apps/fast-notes/fast-notes.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FastNotesService } from './fast-notes.service';
+import { ToastrService } from 'ngx-toastr';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-fast-notes',
@@ -7,9 +10,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FastNotesComponent implements OnInit {
 
-  constructor() { }
+  public formEditNotes: Array<FormGroup> = [];
+  public isLoading = true;
+
+  constructor(private service: FastNotesService,
+    private toastr: ToastrService) { }
 
   ngOnInit() {
+    this.get();
+  }
+
+  public removeNote(): void{
+    console.log('foi');
+    this.formEditNotes.splice(this.formEditNotes.length, 1);
+  }
+
+  public addNotes(): void{
+    this.formEditNotes.push(new FormBuilder().group({
+      id: [this.formEditNotes.length],
+      title: ['', Validators.required],
+      content: ['', Validators.required]
+    }));
+  }
+
+  public get(): void{
+    this.service.getNotes().subscribe(response => {
+      this.generateFormEdit(response);
+    },
+    () => {
+      this.toastr.error('Não foi possível pegar as anotações. Tente novamente.');
+    });
+  }
+
+  private generateFormEdit(data: Array<any>){
+    for(let i = 0; i < data.length; i++){
+      this.formEditNotes[i] = new FormBuilder().group({
+        id: [data[i].id],
+        title: [data[i].title, [Validators.required]],
+        content: [data[i].content, [Validators.required]]
+      });
+    }
+    this.isLoading = false;
+  }
+
+  public post(notes): void{
+    this.service.setNotes(notes);
+  }
+
+  public update(notes): void{
+    this.service.updateNotes(notes);
+  }
+
+  public delete(id: number): void{
+    this.formEditNotes.splice(id, 1);
+    this.service.deleteNotes(id);
   }
 
 }
---
+++ project/src/app/apps/fast-notes/fast-notes.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { ToastrService } from 'ngx-toastr';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FastNotesService {
+
+  private _baseUrl: string = 'http://localhost:3000/notes';
+
+  constructor(private _http: HttpClient,
+    private toastr: ToastrService) { }
+
+  public getNotes(): Observable<any[]> {
+    return this._http.get<any[]>(this._baseUrl);
+  }
+
+  public setNotes(notes: any): void {
+    this._http.post(`${this._baseUrl}/`, notes).subscribe(() => {
+      this.toastr.success('O evento foi salvo com sucesso!');
+    },
+      () => this.toastr.error('Não foi possível salvar o evento. Tente novamente.'));
+  }
+
+  public updateNotes(notes: any): void {
+    this._http.put(`${this._baseUrl}/${notes.id}`, {
+      "id": notes.id,
+      "title": notes.title,
+      "content": notes.content
+    }).subscribe(() => {
+      this.toastr.success('A anotação foi atualizada.');
+    },
+      () => {
+        this.toastr.error('Não foi possível atualizar a anotação. Tente novamente.');
+      }
+    )
+  }
+
+  public deleteNotes(id: number): void{
+    this._http.delete(`${this._baseUrl}/${id}`).subscribe(() => {
+      this.toastr.success('A anotação foi deletada com sucesso.');
+    },
+    () => {
+      this.toastr.error('Não foi possível excluir a anotação. Tente novamente.');
+    })
+  }
+}
